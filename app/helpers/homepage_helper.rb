cf44f4916ed6464aa5f4806fd1e591887a837817
James Halliday <jhallida@indiana.edu>
2019-03-11 16:34:22 -0400

    fix pretty printing

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -2,34 +2,7 @@ module HomepageHelper
 
   # recursively render JSON / XML data in HTML
   def show_data_for(jsondata)
-    if jsondata.nil?
-      return ''
-    end
-    output = "<ul>"
-    jsondata.each do |key, value|
-      if value.is_a?(Hash)
-        output += "<li><b>#{key}:</b>"
-        output += show_data_for(value)
-        output += "</li>"
-      elsif value.is_a?(Array)
-        output += "<li><b>#{key}:</b><ul>"
-        value.each do |value|
-          if value.is_a?(String)
-            output += "<li>#{value}</li>"
-          else
-            output += show_data_for(value)
-          end
-        end
-        output += "</ul></li>"
-      else
-        # make strings that seem to be URL's automatically cliackable
-        if value.to_s.downcase.start_with?("http")
-          value = "<a href='" + value + "'>" + value + "</a>"
-        end
-        output += "<li><b>#{key}:</b> #{value}</li>"
-      end
-    end
-    output += "</ul>"
-    return output.html_safe
+    return JSON.pretty_generate(jsondata).html_safe
   end
+
 end
